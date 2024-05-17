4dd12c4d054323bda8857328e43958c0dc3f9eb1
ninjainfosys78 <technical.ninjainfosys@gmail.com>
2024-05-17 17:16:36 +0545

    text

--- src/Components/TextOption.tsx
+++ src/Components/TextOption.tsx
@@ -1,9 +1,46 @@
-import React from 'react'
+import React from "react";
+import { OptionType } from "../Models/Setting";
 
 const TextPart = () => {
-  return (
-    <div>TextPart</div>
-  )
-}
+  const types: string[] = [
+    "button",
+    "checkbox",
+    "color",
+    "date",
+    "datetime-local",
+    "email",
+    "file",
+    "hidden",
+    "image",
+    "month",
+    "number",
+    "password",
+    "radio",
+    "range",
+    "reset",
+    "search",
+    "submit",
+    "tel",
+    "text",
+    "time",
+    "url",
+    "week",
+  ];
 
-export default TextPart
+  const refactoredType = (): OptionType[]=> types.map((type:string)=> ({label: type, value: type}))
+ 
+
+  return (
+    <>
+      <select name="type" id="type">
+       {
+        refactoredType().map((type)=>
+            <option value={type.value}>{type.label}</option>
+        )
+       }
+      </select>
+    </>
+  );
+};
+
+export default TextPart;
